5c8d3347b418a7203a5cf822521c52ddf9f2072d
Ai Shadhin <aishadhin.ac@gmail.com>
2022-09-07 12:32:01 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { Button } from '@material-tailwind/react';
+import Navbar from './Components/Shared/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          vroomX From aishadhin
-        </a>
-        <h1 className='text-5xl font-semibold uppercase'>hello from tailwind</h1>
-        <Button>Button</Button>
-      </header>
+    <div>
+      <Navbar></Navbar>
     </div>
   );
 }
