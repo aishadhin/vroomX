5aedae6659dcef1be8658db5d60f187f4eb473b1
Heezol06 <shahriarrahmanheezol@gmail.com>
2022-09-07 17:40:20 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-
-import './App.css';
-import Navbar from './Components/Shared/Navbar/Navbar';
+import "./App.css";
+import Home from "./Components/Home/Home";
+import Navbar from "./Components/Shared/Navbar/Navbar";
 
 function App() {
   return (
     <div>
-
       <Navbar>
-
+        <Home></Home>
       </Navbar>
-
     </div>
   );
 }
