64d84e86bd8d051ed48605e84ecf5dcf4d78be56
Heezol06 <shahriarrahmanheezol@gmail.com>
2022-09-18 16:48:09 +0600

    services added

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -1,11 +1,10 @@
-import { Button } from "@material-tailwind/react";
 import React from "react";
 import car from "../../../Assets/Icons/Banner-min.png"
 import { BsArrowRightShort } from 'react-icons/bs';
 
 const Banner = () => {
   return (
-    <section style={{backgroundImage:`url(${car})`}} className="my-[70px] text-white bg-no-repeat bg-cover h-screen mx-w">
+    <section style={{backgroundImage:`url(${car})`}} className="mt-[70px] text-white bg-no-repeat bg-cover h-screen mx-w">
       <div class="grid lg:grid-cols-2 gap-4 max-w-7xl ml-10">
         <div className="py-10 pl-5">
           <h1 className="font-bold">
@@ -14,7 +13,7 @@ const Banner = () => {
           <p className="my-10">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ipsum adipisci facilis animi nostrum quia, praesentium voluptates fuga ut odit, aut maiores libero provident? Reprehenderit, pariatur quas nemo nisi dolores excepturi porro. Laborum nam asperiores alias ab tempora, similique soluta. Sequi veritatis quibusdam saepe beatae impedit esse ratione voluptatem reiciendis?
           </p>
-          <Button className="btn red-bg capitalize rounded border-none px-14 text-white">Subscribe  <BsArrowRightShort className="font-semibold text-2xl"/></Button>
+          <button className="btn red-bg capitalize rounded border-none px-14 text-white">Subscribe  <BsArrowRightShort className="font-semibold text-2xl"/></button>
           
         </div>
         
