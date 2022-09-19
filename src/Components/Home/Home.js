e5368e035a7f7fbe838f7149c42e277d7add364e
Heezol06 <shahriarrahmanheezol@gmail.com>
2022-09-19 14:04:10 +0600

    counter

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import Banner from './Inside-Home/Banner';
-import Brands from './Inside-Home/Brands';
-import Services from './Services/Services';
+import React from "react";
+import Banner from "./Inside-Home/Banner";
+import Brands from "./Inside-Home/Brands";
+import Counter from "./Inside-Home/Counter";
+import Services from "./Inside-Home/Services";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner />
-            <Brands />
-            <Services></Services>
-        </div>
-    );
+  return (
+    <div>
+      <Banner />
+      <Brands />
+      <Services />
+      <Counter></Counter>
+    </div>
+  );
 };
 
 export default Home;
